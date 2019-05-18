1e38493a1c2985a8ff400749f92f8dce3f7a612b
Dharikh <50337336+Dharikh@users.noreply.github.com>
2019-05-18 14:53:11 -0500

    Update Add DHCP to 2 Switches or Routers.py

--- Add DHCP to 2 Switches or Routers.py
+++ Add DHCP to 2 Switches or Routers.py
@@ -1,29 +1,29 @@
 from netmiko import ConnectHandler,redispatch
 
-SW1 = {
+SW01 = {
 'device_type' : 'cisco_ios',
 'ip' : '192.168.238.42',
-'username' : 'dhari',
+'username' : 'cisco',
 'password' : 'cisco123',
 }
 
-routers = [SW1]
+switches = [SW01]
 for routerslist in routers :
-   net_connect = ConnectHandler(**routerslist)
-   config_commands = ['ip dhcp pool VOIC01', 'network 80.80.80.0 255.255.255.0', 'default-route 80.80.80.1']
+   net_connect = ConnectHandler(**Switchesist)
+   config_commands = ['ip dhcp pool DATA01', 'network 80.80.80.0 255.255.255.0', 'default-route 80.80.80.1']
    output = net_connect.send_config_set(config_commands)
    print output
 
-SW2 = {
+SW02 = {
 'device_type' : 'cisco_ios',
 'ip' : '192.168.238.44',
-'username' : 'dhari',
+'username' : 'cisco',
 'password' : 'cisco123',
 }
 
-routers = [SW2]
+switches = [SW02]
 for routerslist in routers :
-   net_connect = ConnectHandler(**routerslist)
-   config_commands = ['ip dhcp pool VOICE02', 'network 90.90.90.0 255.255.255.0', 'default-route 90.90.90.1']
+   net_connect = ConnectHandler(**Switcheslist)
+   config_commands = ['ip dhcp pool DATA02', 'network 90.90.90.0 255.255.255.0', 'default-route 90.90.90.1']
    output = net_connect.send_config_set(config_commands)
    print output
